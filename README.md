1857a46e4fc706a1bd751dec1681763a12e0ef9e
Rohith Saravana <rsaravana@ucdavis.edu>
2025-05-20 22:51:30 -0700

    created setup directories and files

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # ForecastFM
+
+**Dependencies**
+cd frontend/
+npm install
+npm install --save-dev typescript svelte-preprocess @tsconfig/svelte
+npm run dev
+
+cd backend/
+pip install --no-cache-dir -r requirements.txt
---
+++ frontend/src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+    let test = "";
+</script>
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width" />
+    <title>Forecast FM</title>
+</head>
+<body>
+    <p>Testing app.svelte</p>
+</body>
