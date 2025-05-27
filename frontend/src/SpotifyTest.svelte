66029f3db14ff129821f4273d708200aba5bd257
Rohith Saravana <rsaravana@ucdavis.edu>
2025-05-27 12:47:05 -0700

    updated spotify api and added main.ts

---
+++ frontend/src/SpotifyTest.svelte
@@ -0,0 +1,86 @@
+<script lang="ts">
+    const clientId = ""; // get from spotify Dev portal
+    const params = new URLSearchParams(window.location.search);
+    const code = params.get("code"); // if undef then user isn't authorized
+
+    if (!code) {
+        redirectToSpotify(clientId);
+    } else {
+        const accessToken = await getAccessToken(clientId, code);
+        const spotifyData = await getSpotifyData(accessToken);
+        console.log(spotifyData);
+    }
+
+    // vv API Code taken from devloper.spotify.com vv
+    async function redirectToSpotify(clientId: string) {
+        const verifier = generateCodeVerifier(128);
+        const challenge = await generateCodeChallenge(verifier);
+
+        localStorage.setItem("verifier", verifier);
+
+        const params = new URLSearchParams();
+        params.append("client_id", clientId);
+        params.append("response_type", "code");
+        params.append("redirect_uri", "http://127.0.0.1:5173/callback");
+        params.append("scope", "user-read-private user-read-email");
+        params.append("code_challenge_method", "S256");
+        params.append("code_challenge", challenge);
+
+        document.location = `https://accounts.spotify.com/authorize?${params.toString()}`;
+    }
+
+    function generateCodeVerifier(length: number) {
+        let text = "";
+        let possible =
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+        for (let i = 0; i < length; i++) {
+            text += possible.charAt(
+                Math.floor(Math.random() * possible.length),
+            );
+        }
+        return text;
+    }
+
+    async function generateCodeChallenge(codeVerifier: string) {
+        const data = new TextEncoder().encode(codeVerifier);
+        const digest = await window.crypto.subtle.digest("SHA-256", data);
+        return btoa(
+            String.fromCharCode.apply(null, [...new Uint8Array(digest)]),
+        )
+            .replace(/\+/g, "-")
+            .replace(/\//g, "_")
+            .replace(/=+$/, "");
+    }
+
+    // ^^ Code taken from spotify ^^
+
+    async function getAccessToken(clientId: string, code: string) {
+        const verifier = localStorage.getItem("verifier");
+
+        const params = new URLSearchParams();
+        params.append("client_id", clientId);
+        params.append("grant_type", "authorization_code");
+        params.append("code", code);
+        params.append("redirect_uri", "http://127.0.0.1:5173/callback");
+        params.append("code_verifier", verifier!);
+
+        const result = await fetch("https://accounts.spotify.com/api/token", {
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: params,
+        });
+
+        const { access_token } = await result.json();
+        return access_token;
+    }
+
+    async function getSpotifyData(accessToken: string) {}
+</script>
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width" />
+    <title>Forecast FM</title>
+</head>
+<body> </body>
