3e7bf51b5421a21b93efbfe6581fe5fa38fa099d
ethanloinguyen <ethanloinguyen@gmail.com>
2025-06-03 16:01:22 -0700

    still working on refresh token

--- frontend/src/SpotifyTest.svelte
+++ frontend/src/SpotifyTest.svelte
@@ -3,7 +3,27 @@
     // ^ TODO Put this in env file later
     const params = new URLSearchParams(window.location.search);
     const code = params.get("code"); // if undef then user isn't authorized
+
+    const tokenEndpoint = "https://accounts.spotify.com/api/token";
+    const scope = 'user-read-private user-read-email';
  
+    const currentToken = {
+        get access_token() { return localStorage.getItem('access_token'); },
+        get refresh_token() { return localStorage.getItem('refresh_token'); },
+        get expires_in() { return localStorage.getItem('refresh_in') },
+        get expires() { return localStorage.getItem('expires')},
+
+        save: function (response) {
+            const { access_token, refresh_token, expires_in } = response;
+            localStorage.setItem('access_token', access_token);
+            localStorage.setItem('refresh_token', refresh_token);
+            localStorage.setItem('expires_in', expires_in);
+
+            const now = new Date();
+            const expiry = new Date(now.getTime() + (expires_in * 1000));
+            localStorage.setItem('expires', expiry);
+        }
+    };
 
     export async function logIn() {
         console.log("spotify test loaded");
@@ -29,7 +49,7 @@
         params.append("client_id", clientId);
         params.append("response_type", "code");
         params.append("redirect_uri", "http://127.0.0.1:5173/callback");
-        params.append("scope", "user-read-private user-read-email");
+        params.append("scope", scope);
         params.append("code_challenge_method", "S256");
         params.append("code_challenge", challenge);
 
@@ -74,7 +94,7 @@
         params.append("redirect_uri", "http://127.0.0.1:5173/callback");
         params.append("code_verifier", verifier!);
 
-        const result = await fetch("https://accounts.spotify.com/api/token", {
+        const result = await fetch(tokenEndpoint, {
             method: "POST",
             headers: { "Content-Type": "application/x-www-form-urlencoded" },
             body: params,
@@ -83,6 +103,24 @@
         const { access_token } = await result.json();
         return access_token;
     }
+
+    async function refreshToken() {
+
+        const params = new URLSearchParams();
+        params.append("client_id", clientId);
+        params.append("grant_type", 'refresh_token');
+        params.append("refresh_token", currentToken.refresh_token);
+
+        const response = await fetch(tokenEndpoint, {
+        method: 'POST',
+        headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: params,
+    });
+
+  return await response.json();
+}
 
     async function getSpotifyData(accessToken: string) {}
 </script>
