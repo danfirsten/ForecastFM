65c2484ff50d3a35991657c468072c3104bc1159
Nadav Weinberger <nadav792001@gmail.com>
2025-06-03 18:06:35 -0700

    kinda works

--- frontend/src/LocationPage.svelte
+++ frontend/src/LocationPage.svelte
@@ -1,5 +1,53 @@
-<script>
+<script lang="ts">
   import './LocationPage.css';
+
+  let status: HTMLParagraphElement | null = null;
+
+  function geoFindMe() {
+        console.log("Pressed");
+        
+        if (!status) return;
+        
+        // temperature.textContent = "";
+
+        async function success(position: GeolocationPosition) {
+            if (!status) return;
+            
+            const latitude = position.coords.latitude;
+            const longitude = position.coords.longitude;
+
+            let api_url = `http://127.0.0.1:5000/weather/${latitude}/${longitude}`;
+            console.log(api_url);
+            let resp = await fetch(api_url);
+            let resp_json = await resp.json();
+
+            let data = resp_json["current"];
+
+            localStorage.setItem("weatherCode",data["current"]);
+            localStorage.setItem("temperature",data["temperature"]+"°F");
+
+            status.textContent = data["current"] + " " + data["temperature"] + "°F";
+
+            // weatherCode.textContent = `Code: ${data["weather_code"]}`;
+            // temperature.textContent = `${data["temperature"]}°F`;
+            // temperature.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
+        }
+
+        function error() {
+            if (!status) return;
+
+            status.style.color = "red";
+            status.textContent = "Unable to retrieve your location";
+        }
+
+        if (!navigator.geolocation) {
+            status.style.color = "red";
+            status.textContent = "Geolocation is not supported by your browser";
+        } else {
+            status.textContent = "Locating…";
+            navigator.geolocation.getCurrentPosition(success, error);
+        }
+  }
 </script>
 
 <div class="location-bg">
@@ -21,7 +69,8 @@
           <svg width="22" height="22" fill="none" stroke="#023047" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24"><circle cx="11" cy="11" r="8"/><line x1="21" y1="21" x2="16.65" y2="16.65"/></svg>
         </button>
       </div>
-      <a href="#" class="current-location-link">Use my current location</a>
+      <a href="#" class="current-location-link" on:click={geoFindMe}>Use my current location</a>
+      <p bind:this={status}></p>
     </div>
   </div>
 </div> 
