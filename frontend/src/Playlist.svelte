e8884bbdd8643b6dc31e45fab795afe284efb21c
avigailsg <105086493+avigailsg@users.noreply.github.com>
2025-06-11 10:21:03 -0700

    Update Playlist.svelte

--- frontend/src/Playlist.svelte
+++ frontend/src/Playlist.svelte
@@ -90,22 +90,10 @@
         push("/location");
     }
 
-
-    let weatherCode = 0; 
-    $: weatherIcon = getWeatherIcon(weatherCode); 
-    function getWeatherIcon(code) {
-        if (code === 0) return "Sunny";
-        if ([1, 2, 3].includes(code)) return "Cloudy";
-        if ([45, 48].includes(code)) return "Fog";
-        if ([51, 53, 55, 56, 57].includes(code)) return "Drizzle";
-        if ([61, 63, 65, 66, 67, 80, 81, 82].includes(code)) return "Rain";
-        if ([71, 73, 75, 77, 85, 86].includes(code)) return "Snow";
-        if ([95, 96, 99].includes(code)) return "Thunderstorm";
-        return "Image"; 
-    }
-
-    let location = 'Davis, California';
-    let temperature = '88\u00B0F Sunny';
+    let location = localStorage.getItem("location");
+    let weather = localStorage.getItem("weather");
+    let temperature = localStorage.getItem("temperature");
+    let fullWeather = `${temperature}\u00B0F ${weather}`;
 </script>
 
 <div>
