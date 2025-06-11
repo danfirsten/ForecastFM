b291af2881ccace829613bebc751abb724c5332c
Rohith Saravana <rsaravana@ucdavis.edu>
2025-06-11 09:49:16 -0700

    change location button routes to location page now)

--- frontend/src/Playlist.svelte
+++ frontend/src/Playlist.svelte
@@ -84,7 +84,7 @@
     }
 
     async function handleChangeLocation() {
-        await logOut();
+        //await logOut();
         await refreshTokenClick();
         // await logIn();
         push("/location");
@@ -148,7 +148,6 @@
                             width="100%"
                             height="100"
                             frameBorder="0"
-                            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
                         ></iframe>
                     </div>
                 {/each}
